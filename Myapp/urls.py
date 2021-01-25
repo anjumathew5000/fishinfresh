99979b5f990959948c85cd32010d75c86eaf0449
anju mathew <anjumathew1511@gmail.com>
2021-01-25 10:45:09 +0530

    AddingFiles

--- Myapp/urls.py
+++ Myapp/urls.py
@@ -3,5 +3,29 @@ from django.urls import path
 from Myapp import views
 
 urlpatterns = [
-   path('index/',views.index,name="index")
+   path('index/',views.index,name="index"),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   #website
+   path('',views.home,name="home")
 ]
--- Myapp/views.py
+++ Myapp/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'adminp/index.html')
+
+
+
+
+
+
+
+#webiste
+
+def home(request):
+    return render(request,'website/index.html')
